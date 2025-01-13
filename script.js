48a9e38555495bd844b870b96225520c49b243b7
Sergio Torres <sergio.torres@idr.edu.mx>
2025-01-13 13:49:26 -0600

    feat: funcion solicitarYCambiarNombre

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function solicitarYCambiarNombre (){
+  const nombre = prompt("¿Cuál es tu nombre?");
+  // uso de la api DOM para cambiar el texto de un elemento
+  document.getElementById("texto-saludo").innerText = "Hola " + nombre;
+
+}
